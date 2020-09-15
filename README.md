d22361996ac209a90b1d4d92de2f65f8446beb1e
Radoslav <rudix.lab@gmail.com>
2020-09-15 12:17:07 +0300

    c

---
+++ README.md
@@ -0,0 +1,43 @@
+<h1 align="center">:smile_cat:<tt>octochat</tt></h1>
+
+<blockquote align="center">Privately message with your GitHub followers</blockquote>
+
+<p align="center">
+  <a href="https://github.com/github-developer/octochat-aws"><img alt="GitHub Actions status" src="https://github.com/github-developer/octochat-aws/workflows/Build/badge.svg"></a>
+</p>
+
+## Usage
+
+<img width="1072" alt="example-octochat" src="https://user-images.githubusercontent.com/27806/69747446-020e6900-110c-11ea-9a4b-41fc59a9bb6f.png">
+
+[Login with your GitHub account](https://octochat.dev/login) to start messaging privately with your GitHub followers.
+
+## About
+
+This app is intended _for demonstration use only_, and is deployed using [Amazon Web Services](https://aws.amazon.com/). The architecture is explained at a high-level [here](docs/ARCHITECTURE.md).
+
+## Development
+
+Create a `.env` file based on [the included example template](.env.example).
+
+Build a development image from [`Dockerfile.develop`](Dockerfile.develop):
+
+```shell
+docker build -t octochat -f Dockerfile.develop .
+```
+
+Use the included [`server.sh`](script/server.sh) script to run the development server in a container:
+
+```shell
+./script/server.sh
+```
+
+The application will be available at [http://localhost:49160/](http://localhost:49160/).
+
+## Contributions
+
+We welcome contributions! See how to [contribute](docs/CONTRIBUTING.md).
+
+## License
+
+[MIT](LICENSE.md)
---
+++ app/lib/model.js
@@ -0,0 +1,66 @@
+// https://aws.amazon.com/sdk-for-node-js/
+const aws = require('aws-sdk');
+const lambda = new aws.Lambda();
+const utilities = require('./utilities');
+
+// Callback to Promise wrapper of lambda.invoke
+const invokeLambda = async (params) => {
+  // Convert callback to promise for easier handling
+  return new Promise((resolve) => {
+    // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Lambda.html#invoke-property
+    lambda.invoke(params, (err, data) => {
+      resolve({ err, data });
+    });
+  });
+};
+
+// send a message
+const messageAdd = async (msgObj) => {
+  const params = {
+    FunctionName: 'message_add',
+    Payload: `{"toId": ${msgObj.toId}, "to": "${msgObj.to}", "fromId": ${msgObj.fromId}, "from": "${msgObj.from}", "message": "${msgObj.message}"}`
+  };
+
+  return invokeLambda(params);
+};
+
+// list the 50 most recently received messages
+const messagesReceivedList = async (toId) => {
+  const params = {
+    FunctionName: 'messages_received_list',
+    Payload: `{
+            "toId": ${toId}
+        }`
+  };
+
+  return utilities.cleanMessages(await invokeLambda(params));
+};
+
+// list the 50 most recently sent messages
+const messagesSentList = async (fromId) => {
+  const params = {
+    FunctionName: 'messages_sent_list',
+    Payload: `{
+            "fromId": ${fromId}
+        }`
+  };
+
+  return utilities.cleanMessages(await invokeLambda(params));
+};
+
+// get messages between two user ids
+const messagesBetween = async (user1, user2) => {
+  // gather messages from user2 sent to user1
+  const received = (await messagesReceivedList(user1)).filter(msg => parseInt(msg.fromId, 10) === parseInt(user2, 10));
+  // gather messages to user2 sent by user1
+  const sent = (await messagesSentList(user1)).filter(msg => parseInt(msg.toId, 10) === parseInt(user2, 10));
+  // combine into one array and sort by received date
+  return [].concat(received, sent).sort((a, b) => new Date(a.receivedAt) - new Date(b.receivedAt));
+};
+
+module.exports = {
+  messageAdd,
+  messagesReceivedList,
+  messagesSentList,
+  messagesBetween
+};
