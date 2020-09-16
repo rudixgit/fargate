09952db10fcf05eb9f07cb397b4cb7609b091e6e
Radoslav <rudix.lab@gmail.com>
2020-09-16 13:13:06 +0300

    c

--- app/server.js
+++ app/server.js
@@ -1,22 +1,11 @@
-const { app } = require("./lib/app");
-const handlers = require("./lib/handlers");
-const logger = require("./lib/logger");
+const express = require("express");
+const app = express();
+const port = 80;
 
-// set up handlers for each route
-app.get("/", handlers.getRoot);
-app.get("/messages/:userId", handlers.getConversation);
-app.get("/sent", handlers.getSent);
-app.get("/login", handlers.login);
-app.get("/logout", handlers.logout);
-app.get("/oauth", handlers.completeOauth);
-app.get("/health-check", handlers.healthCheck);
-app.post("/messages", handlers.validateMessage(), handlers.postMessage);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-// listen for requests :)
-const listener = app.listen(
-  process.env.PORT || 80,
-  process.env.HOST || "0.0.0.0",
-  () => {
-    logger.info(`Your app is listening on port ${listener.address().port}`);
-  }
-);
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
